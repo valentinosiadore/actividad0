b87f77fcd5fb67d9d06bca53cdfce47979808230
valentinosiadore <valentinosiadore@gmail.com>
2025-04-12 20:03:42 -0300

    [Parte 3.5] Generar un error 404

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
-from django.template import loader
 from django.shortcuts import render
+from django.http import Http404
 from .models import Question
 
 def index(request):
@@ -9,7 +9,11 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    return HttpResponse(f"Esta es la pregunta %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("La pregunta no existe.")
+    return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
     response = "Estas viendo los resultados de la pregunta %s."
