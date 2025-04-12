e67481f1987a932e461d82dbe480fcbd5a0d219c
valentinosiadore <valentinosiadore@gmail.com>
2025-04-12 19:02:24 -0300

    [Parte 3.2] Escribir más vistas

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,3 +2,14 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hola mundo. Estas son las encuestas de la aplicación.")
+
+def detail(request, question_id):
+    return HttpResponse(f"Esta es la pregunta %s." % question_id)
+
+def results(request, question_id):
+    response = "Estas viendo los resultados de la pregunta %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse(f"Estas votando por la pregunta %s." % question_id)
+
